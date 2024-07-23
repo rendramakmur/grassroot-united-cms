32e2679977e39524d65b89189a74f5eaf712435e
Rendra <rendra@SMI266-REN.local>
2024-07-23 13:56:50 +0700

    RENDRA - Add documentation plugin and auto-generated user number

--- src/index.js
+++ src/index.js
@@ -16,5 +16,31 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    // Registering a subscriber
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::users-permissions.user'], // Target the user model
+
+      beforeCreate(event) {
+        const { data } = event.params;
+        data.userNumber = generateUserNumber();
+        console.log('Generated userNumber:', data.userNumber); // Debug log
+      },
+    });
+
+    // Generic subscribe for generic handling
+    strapi.db.lifecycles.subscribe((event) => {
+      if (event.action === 'beforeCreate' && event.model.uid === 'plugin::users-permissions.user') {
+        const { data } = event.params;
+        data.userNumber = generateUserNumber();
+        console.log('Generated userNumber:', data.userNumber); // Debug log
+      }
+    });
+  },
+};
+
+// Helper function to generate user number
+const generateUserNumber = () => {
+  const randomNumber = Math.floor(100000 + Math.random() * 900000);
+  return `GRU-${randomNumber}`;
 };
